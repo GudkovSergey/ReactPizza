f63bb6a4194dd2b043664b4a4bd8eb45239d9507
GudkovSergey <gudkow-sergey@mail.ru>
2023-01-06 19:15:16 +0500

    refactoring html to react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './scss/app.scss';
+import Header from "./scss/components/Header/Header";
+import Categories from "./scss/components/Categories/Categories";
+import Sort from "./scss/components/Sort/Sort";
+import PizzaBlock from "./scss/components/PizzaBlock/PizzaBlock";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="wrapper">
+            <Header/>
+            <div className="content">
+                <div className="container">
+                    <div className="content__top">
+                        <Categories/>
+                        <Sort/>
+                    </div>
+                    <h2 className="content__title">Все пиццыяя</h2>
+                    <div className="content__items">
+                        <PizzaBlock/>
+                        <PizzaBlock/>
+                        <PizzaBlock/>
+                        <PizzaBlock/>
+                        <PizzaBlock/>
+                        <PizzaBlock/>
+                        <PizzaBlock/>
+                        <PizzaBlock/>
+                        <PizzaBlock/>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    );
 }
 
 export default App;
