5662a742d9859f92d1f254474637cbffe4107e28
GudkovSergey <gudkow-sergey@mail.ru>
2023-01-12 22:14:15 +0500

    Search component

--- src/ReactComponents/Header/Header.jsx
+++ src/ReactComponents/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import logoSvg from "../../assets/img/pizza-logo.svg";
 import {Link} from "react-router-dom";
+import Search from "../Search/Search";
 const Header =()=> {
    return(
        <div className="header">
@@ -14,6 +15,7 @@ const Header =()=> {
                 </div>
             </div>
             </Link>
+            <Search/>
             <div className="header__cart">
                 <Link to="/cart" className="button button--cart">
                     <span>520 ₽</span>
